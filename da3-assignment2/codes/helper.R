697482dd5cd7b50e6941f8695f16e1c760ab5322
gulacsydominik@gmail.com <dgulacsy>
2021-02-08 12:28:37 +0100

    add helper funcions script

--- da3-assignment2/codes/helper.R
+++ da3-assignment2/codes/helper.R
@@ -10,3 +10,11 @@
 # Set Plot Size Dimensions
 mywidth=7.5
 myheight=5.62
+
+# Create Feature Selection Schema File creator function
+fss <- function(df) {
+  fss_df<-data.frame(vars=colnames(df),example=as.character(df[1,]),use=NA,notes=NA)
+  write_csv(fss_df,"data/raw/feature_selection_schema.csv",na = "")
+  print(df)
+}
+
--- da3-assignment2/codes/high-growth-firms-prediction.R
+++ da3-assignment2/codes/high-growth-firms-prediction.R
@@ -2,10 +2,8 @@
 # DA3 Assignment 2 
 # Analysis/Prediction Script
 # 
-# GOAL:of high-growth firms
-# Task is to help a company operating small and mid-size apartments hosting 2-6 guests. 
-# The company is set to price their new apartments not on the market. 
-# Build a price prediction model.
+# GOAL:
+# of high-growth firms
 #
 # Data Source
 # Website: https://osf.io/b2ft9/
@@ -25,6 +23,8 @@ library(xtable)
 library(knitr)
 library(skimr)
 
+source("codes/helper.R")
+
 options(digits=3)
 
 # Import data -------------------------------------------------------------
@@ -32,9 +32,13 @@ df<-read_csv("data/raw/cs_bisnode_panel.csv")
 skim(df)
 
 # Feature Selection -------------------------------------------
-fss_df<-data.frame(vars=colnames(df),example=as.character(df[1,]),use=NA,notes=NA)
-write_csv(fss_df,"data/raw/feature_selection_schema.csv",na = "")
+# Create Feature Selection Schema File
+fss(df)
+
+# Import file containing feature selection strategy
+fss <- read_csv("data/raw/feature_selection_schema.csv")
 
 # Import file containing feature selection strategy
 fss <- read_csv(paste0(data_dir,"raw/feature_selection_strat.csv"))
-
+unique(df$year)
+table(df$year)
