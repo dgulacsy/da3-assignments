4e1a36444fe898e3a6a8e55bb05a7528615179ce
szutsattila <szuts.attila97@gmail.com>
2021-02-10 09:03:32 +0100

    set default value for webhook function

--- da3-assignment2/codes/helper.R
+++ da3-assignment2/codes/helper.R
@@ -31,7 +31,7 @@ vss <- function(df) {
 # discord-webhook function ------------------------------------------------
 
 # define webhook function
-send_message <- function(webhookurl, my_text) {
+send_message <- function(webhookurl = url, my_text = text) {
   library(jsonlite)
   library(rvest)
   
@@ -44,8 +44,7 @@ send_message <- function(webhookurl, my_text) {
 
 # read in credentials
 creds <- read.delim('/mnt/DATA/Projects/BA-20-21/danalysis-3/data/creds.txt', sep = ',') # change the path to your creds.txt file
-webhookurl <- as.character(creds[creds$key == 'webhookurl', 2])
+url <- as.character(creds[creds$key == 'webhookurl', 2])
 user <- as.character(creds[creds$key == 'user', 2])
 
-my_text <- paste0('hello ', user, ' your model has finished training')
-
+text <- paste0('hello ', user, ' your model has finished training')
