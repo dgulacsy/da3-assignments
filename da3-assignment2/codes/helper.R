e639c6e0ae7ae3c36608224b90e7465705a3c433
gulacsydominik@gmail.com <dgulacsy>
2021-02-11 19:07:36 +0100

    add classification and threshold search

--- da3-assignment2/codes/helper.R
+++ da3-assignment2/codes/helper.R
@@ -30,7 +30,29 @@ vss <- function(df) {
   }
 }
 
-
+#createLossPlot <- function(r, best_coords, file_name,  mywidth_large=12, myheight_large = 9) {
+createLossPlot <- function(r, best_coords, file_name,  myheight_small = 5.625, mywidth_small = 7.5) {
+  t <- best_coords$threshold[1]
+  sp <- best_coords$specificity[1]
+  se <- best_coords$sensitivity[1]
+  n <- rowSums(best_coords[c("tn", "tp", "fn", "fp")])[1]
+  
+  all_coords <- coords(r, x="all", ret="all", transpose = FALSE)
+  all_coords <- all_coords %>%
+    mutate(loss = (fp*FP + fn*FN)/n)
+  l <- all_coords[all_coords$threshold == t, "loss"]
+  
+  loss_plot <- ggplot(data = all_coords, aes(x = threshold, y = loss)) +
+    geom_line(color="green", size=0.7) +
+    scale_x_continuous(limits = c(0, 1), breaks = seq(0, 1, by = 0.1)) +
+    geom_vline(xintercept = t , color = "red" ) +
+    annotate(geom = "text", x = t, y= min(all_coords$loss),
+             label=paste0("best threshold: ", round(t,2)),
+             colour="blue", angle=90, vjust = -1, hjust = -0.5, size = 7) +
+    annotate(geom = "text", x = t, y= l,
+             label= round(l, 2), hjust = -0.3, size = 7)
+  loss_plot
+}
 
 # discord-webhook function ------------------------------------------------
 
